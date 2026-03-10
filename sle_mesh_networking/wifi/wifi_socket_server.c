2693c2a34cb77bfbae1a02e39b098882b597ea63
wellwhz <wellwhz@qq.com>
2026-03-10 16:46:22 +0800

    fix: wifi_img_server栈增回6KB给lwip留更多余量(总节省10KB堆)

--- sle_mesh_networking/wifi/wifi_socket_server.c
+++ sle_mesh_networking/wifi/wifi_socket_server.c
@@ -15,7 +15,7 @@
 #include <string.h>
 
 #define SOCK_LOG    "[wifi sock]"
-#define SERVER_TASK_STACK   0x1000  /* 4KB: waterline=0x498(1.2KB), 4KB 留 3x 余量。释放 4KB 堆给 WiFi+BT OAL */
+#define SERVER_TASK_STACK   0x1800  /* 6KB: waterline=0x498(1.2KB), lwip recv/send 需要额外栈空间。释放 2KB 堆给 WiFi+BT OAL */
 #define SERVER_TASK_PRIO    (osPriority_t)(15)
 
 /**
